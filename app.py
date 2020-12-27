5cc488e0366e38991e4561f517dac6d38fb9dc55
pandeyaah <pandeyaah@gmail.com>
2020-12-26 23:19:29 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+
+import streamlit as st
+st.title("From SupportVectors AI Research Lab  ....")
+st.image('https://www.supportvectors.io/gaia/pluginfile.php/1/theme_lambda/logo/1599599177/support-vectors-logo.gif',use_column_width = True)
+
+st.balloons()
+
